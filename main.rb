e6a2965ad08985b2ddb51dafa8358b2c66efe119
Guest Account <Guest@Maria-Jemimah.local>
2014-05-11 23:17:28 +0800

    final commit

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require './boot.rb'
+require './money_calculator.rb'
 
 # ROUTES FOR ADMIN SECTION
 get '/admin' do
@@ -43,3 +44,69 @@ get '/delete_product/:id' do
   redirect to '/admin'
 end
 # ROUTES FOR ADMIN SECTION
+
+get '/about' do
+  erb :about
+end
+
+get '/' do
+  products = Item.all
+  @products = products.sample(10)
+  erb :index
+end
+
+get '/products' do
+  @products = Item.all
+  erb :products
+end
+
+get '/buy/:id' do
+  @product = Item.find(params[:id])
+  erb :buy
+end
+
+post '/checkout' do
+  thousands = params[:thousands].to_i
+  five_hundreds = params[:five_hundreds].to_i
+  hundreds = params[:hundreds].to_i
+
+  fifties = params[:fifties].to_i
+  twenties = params[:twenties].to_i
+  tens = params[:tens].to_i
+  fives = params[:fives].to_i
+  ones = params[:ones].to_i
+  quantity = params[:quantity].to_i
+  product = Item.find(params[:id])
+  price = product.price.to_i
+  cost = price * quantity
+  @p = product
+  @cost = cost
+  money = (ones * 1) + (fives * 5) + (tens * 10) + (twenties * 20) + (fifties * 50) + (hundreds * 100) + (five_hundreds * 500)  + (thousands * 1000)
+  @money = money
+
+  x = MoneyCalculator.new(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
+  
+  
+  change = x.change(cost)
+  @thousands = change[:thousands]
+  @fh = change[:five_hundreds]
+  @hundreds = change[:hundreds]
+  @fifties = change[:fifties]
+  @twenties = change[:twenties]
+  @fives = change[:fives]
+  @tens = change[:tens]
+  @ones = change[:ones]
+
+  @total = @thousands  * 1000 + @fh * 500 + @hundreds * 100  + @fifties*50 + @twenties*20 + @fives*5 + @tens*10 + @ones*1
+  if (@cost > @money)
+	erb :failure
+  else
+  product.update_attributes!(
+    quantity: product.quantity - quantity,
+    sold: product.sold + quantity
+  )
+
+
+  erb :checkout
+end
+end
--- money_calculator.rb
+++ money_calculator.rb
@@ -1,12 +1,36 @@
 class MoneyCalculator
-
+  attr_accessor :ones, :fives, :tens, :twenties, :fifties, :hundreds, :five_hundreds, :thousands, :money
   def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
-    # each parameter represents the quantity per denomination of money
-    # these parameters can be assigned to instance variables and used for computation
-
-    # add a method called 'change' that takes in a parameter of how much an item costs
-    # and returns a hash of how much change is to be given
-    # the hash will use the denominations as keys and the amount per denomination as values
-    # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
+    self.ones = ones
+    self.fives = fives
+    self.tens = tens
+    self.twenties = twenties
+    self.fifties = fifties
+    self.hundreds = hundreds
+    self.five_hundreds = five_hundreds
+    self.thousands = thousands
+    self.money = (ones * 1) + (fives * 5) + (tens * 10) + (twenties * 20) + (fifties * 50) + (hundreds * 100) + (five_hundreds * 500)  + (thousands * 1000)
+    @money = money
   end
+
+NOTES = { :thousands => 1000, :five_hundreds => 500, :hundreds => 100,
+           :fifties => 50, :twenties => 20, :tens => 10, 
+           :fives => 5, :ones => 1 }
+def change(x)
+  cost = x
+  money = @money
+  change = money - cost
+
+  Hash.new { |hash, key| hash[key] = 0 }.tap do |notes| 
+    NOTES.each do |name, value|
+      while change >= value
+        change -= value
+        notes[name] += 1
+      end
+    end
+  end
+end
+  
+
+
 end
